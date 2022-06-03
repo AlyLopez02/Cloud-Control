12fd5a8a93aeda5b634e90a059634ffb473cc1be
AlyLopez02 <alylopez0217@gmail.com>
2022-06-03 08:03:54 -0400

    responsiveness update 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,24 @@
 // add the following classes to the recent buttons: city-button btn btn-outline-info btn-block
 // also create a <br> above each button
+
+
+
+
+
+var apiKey = "9d9859759466e90e3c8754b872876ef0";
+
+var city = document.getElementById("user-input").value;
+
+
+
+
+
+
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+// need to change this to fit the link above
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+
+// fetch(queryURL)
+
